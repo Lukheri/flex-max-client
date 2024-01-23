1aa25956b36199f3aff913a7bfe4e34628bac5c0
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-01-24 00:14:22 +0800

    Added individual exercise page with suggested exercises

--- app/components/ExerciseCard.tsx
+++ app/components/ExerciseCard.tsx
@@ -1,21 +1,33 @@
+"use client"
 import React from 'react'
 import { Exercise } from '../constants/types'
+import { useExerciseStore } from '../store/exercise'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 const ExerciseCard = ({exercise, isLoading}: { isLoading: boolean, exercise: Exercise }) => {
-  return (
-    <>
-        {!isLoading ? <div className="card w-96 bg-slate-400 shadow-xl text-black glass overflow-auto">
-            <div className="card-body">
-                <h2 className="card-title capitalize">{exercise.name}</h2>
-                <p className='h-[200px] text-ellipsis overflow-auto no-scrollbar'>{exercise.instructions}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-outline text-black">Add</button>
-                    <button className="btn btn-neutral">View Exercise</button>
+    const { setExercise } = useExerciseStore()
+    const router = useRouter()
+
+    const handleViewExercise = () => {
+        setExercise(exercise)
+        router.push(`/exercises/${exercise.name}`)
+    }
+
+    return (
+        <>
+            {!isLoading ? <div className="card w-96 bg-slate-400 shadow-xl text-black glass overflow-auto">
+                <div className="card-body">
+                    <h2 className="card-title capitalize">{exercise.name}</h2>
+                    <p className='h-[200px] text-ellipsis overflow-auto no-scrollbar'>{exercise.instructions}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-outline text-black">Add</button>
+                        <button onClick={handleViewExercise} className="btn btn-neutral">View Exercise</button>
+                    </div>
                 </div>
-            </div>
-        </div> : <div className="skeleton w-96 h-[250px] bg-slate-400"></div>}
-    </>
-  )
+            </div> : <div className="skeleton w-96 h-[250px] bg-slate-400"></div>}
+        </>
+    )
 }
 
 export default ExerciseCard
---
+++ app/store/exercise.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+import { Exercise } from "../constants/types";
+
+interface ExerciseState {
+    exercise: Exercise,
+    setExercise: (exercise: Exercise) => void
+}
+
+export const useExerciseStore = create<ExerciseState>()((set) => ({
+    exercise: {
+        name: "",
+        type: "",
+        muscle: "",
+        equipment: "",
+        difficulty: "",
+        instructions: "",
+    },    
+    setExercise : (exercise: Exercise) => set({exercise})
+}))
+
+
