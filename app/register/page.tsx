e76e869c279de9aa8955f79063c089cce9513f39
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-01-24 21:04:31 +0800

    Existing user validation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -37,6 +37,21 @@ const RegisterPage = () => {
     }
 
     try {
+      const resUserExists = await fetch('api/userExists', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      })
+
+      const { user } = await resUserExists.json()
+
+      if (user) {
+        setValidationError('User already exists.')
+        return
+      }
+
       const res = await fetch('api/register', {
         method: 'POST',
         headers: {
