a3c9744f9b558be8631b8253878c04a502f1b943
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-01-25 22:43:13 +0800

    Added view routine route

--- app/api/routine/[id]/route.ts
+++ app/api/routine/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import { connectMongoDB } from "@/lib/mongodb";
+import { ObjectId } from "mongoose";
 import Routine from "@/models/routine"
 
 export async function DELETE(req: NextRequest,{ params }: { params: { id: number } } ){
@@ -12,6 +13,20 @@ export async function DELETE(req: NextRequest,{ params }: { params: { id: number
     } catch (error) {
         return NextResponse.json(
             {message: "Error creating routine"}, {status: 500}
+        )
+    }
+}
+
+export async function GET(req: NextRequest,{ params }: { params: { id: number } } ){
+    try {
+        await connectMongoDB()
+
+        const data = await Routine.findById(params.id)
+        
+        return NextResponse.json({message: "Routine fetched", data: data}, {status:201})
+    } catch (error) {
+        return NextResponse.json(
+            {message: "Error getting routine"}, {status: 500}
         )
     }
 }
--- app/routines/[id]/page.tsx
+++ app/routines/[id]/page.tsx
@@ -1,36 +1,52 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import { useRoutineStore } from '@/stores/routine'
-import { Exercise } from '@/app/constants/types'
+import { Exercise, Routine } from '@/app/constants/types'
 import ExerciseCard from '@/components/ExerciseCard'
 
-const RoutineDetails = ({ params }: { params: { name: string } }) => {
-  //   const { viewRoutine } = useRoutineStore()
-  const routine = {
-    name: 'test',
-    description: 'ha',
-    exercises: [],
+const RoutineDetails = ({ params }: { params: { id: any } }) => {
+  const [routine, setRoutine] = useState<Routine>()
+  const { viewRoutine } = useRoutineStore()
+
+  const getRoutine = async () => {
+    try {
+      const response = await fetch(`/api/routine/${params.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.status === 201) {
+        const data = await response.json()
+        setRoutine(data.data)
+      } else {
+        throw new Error('Request failed')
+      }
+    } catch (error) {
+      console.log('Error fetching routine', error)
+    }
   }
 
-  //   const routine = viewRoutine()
+  useEffect(() => {
+    getRoutine()
+  }, [params.id])
 
   return (
     <>
       <div className='flex justify-center p-12'>
         <div className='card glass max-w-[50%] bg-slate-400 p-8 text-black shadow-xl'>
           <div className='card-body'>
-            <h1 className='card-title capitalize'>{routine.name}</h1>
-            <p>{routine.description}</p>
+            <h1 className='card-title capitalize'>{routine?.name}</h1>
+            <p>{routine?.description}</p>
           </div>
-          {/* <div className='card-actions justify-end'>
-            <button className='btn btn-neutral'>Add exercise to routine</button>
-          </div> */}
         </div>
       </div>
       <div className='p-12'>
         <div className='text-[44px]'>Exercises</div>
         <div className='flex flex-wrap items-center justify-around gap-4 px-12'>
-          {routine.exercises.length !== 0 ? (
-            routine.exercises.map((exercise: Exercise, index: number) => (
+          {!!routine ? (
+            routine?.exercises?.map((exercise: Exercise, index: number) => (
               <ExerciseCard
                 key={exercise.name + index}
                 exercise={exercise}
